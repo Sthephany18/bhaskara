964638098957b9c99d248088c0b6aadf6801fc4c
Sthephany18 <129075259+Sthephany18@users.noreply.github.com>
2023-04-11 07:57:25 -0300

    Add files via upload

---
+++ Baskara/src/baskara/Baskara.java
@@ -0,0 +1,18 @@
+package baskara;
+
+
+public class Baskara {
+
+    
+    public static void main(String[] args) {
+       Calculo c1 = new Calculo();
+       c1.setA(2);
+       c1.setB(1);
+       c1.setC(-5);
+       c1.ValorDelta();
+       c1.Valorx1();
+       c1.Valorx2();
+       c1.Status();
+    
+}
+}
